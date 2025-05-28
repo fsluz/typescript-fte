ff69ba1ab7d5b9207706acd759c3a80526267737
fsluz <felipe.luzz86@gmail.com>
2025-05-28 12:01:25 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-function saudar(nome: string): string {
-  return `Olá, ${nome}! Bem-vindo(a) ao TypeScript.`;
-}
-
-let usuario: string = "Estudante";
-console.log(saudar(usuario));
-
-// Tente introduzir um erro de tipo para ver o VS Code e o compilador agindo:
-// usuario = 123; // Isso mostrará um erro no VS Code
+// CÓDIGO PRINCIPAL ONDE VAI RECEBER TODAS AS IMPORTAÇÕES
